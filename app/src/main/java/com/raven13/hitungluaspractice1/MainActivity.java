9dfccb87d66370a846aaf97f8527badc5d07a71e
Ismail Rohaga <azh.zhafir@gmail.com>
2017-01-25 20:53:48 +0700

    trycatch
    pake try catch

--- app/src/main/java/com/raven13/hitungluaspractice1/MainActivity.java
+++ app/src/main/java/com/raven13/hitungluaspractice1/MainActivity.java
@@ -2,6 +2,7 @@ package com.raven13.hitungluaspractice1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -30,19 +31,19 @@ public class MainActivity extends AppCompatActivity {
         count.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(edtPanjang.getText().toString().equals("") || edtLebar.getText().toString().equals("")) {
-                    Toast.makeText(MainActivity.this, "Input Kosong..Tolong masukan input", Toast.LENGTH_SHORT).show();
-                }
-                else {
+
+                try{
                     String panjang = edtPanjang.getText().toString().trim();
-                    String lebar = edtPanjang.getText().toString().trim();
+                    String lebar = edtLebar.getText().toString().trim();
 
                     double p = Double.parseDouble(panjang);
                     double l = Double.parseDouble(lebar);
 
                     double luas = p * l;
-                    Toast.makeText(MainActivity.this, "Luasnya adalah " + luas , Toast.LENGTH_SHORT).show();
                     result.setText("" + luas);
+
+                }catch (NumberFormatException e){
+                    Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
                 }
             }
         });
